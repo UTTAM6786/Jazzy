f69be9a562ed8e00544f833d8b949757992df251
UTTAM RATHORE <uk977061@gmail.com>
2024-10-19 22:42:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = getenv("API_HASH")
 # ------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","@UTTAM470")
+OWNER_USERNAME = getenv("OWNER_USERNAME","UTTAM470")
 # --------------------------------------------------------
 BOT_USERNAME = getenv("BOT_USERNAME" , "Jazzy_music_bot")
 # --------------------------------------------------------
