24d033da46c4be97e93b0d75e0b4d362e3267bab
UTTAM RATHORE <uk977061@gmail.com>
2024-10-19 22:46:23 +0530

    Update start.py

--- BABYMUSIC/utils/inline/start.py
+++ BABYMUSIC/utils/inline/start.py
@@ -3,6 +3,7 @@ from pyrogram.types import InlineKeyboardButton, Message
 import config
 import asyncio
 from BABYMUSIC import app
+from config import BOT_USERNAME
 
 
 # Start panel for inline buttons
@@ -10,7 +11,7 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"], url=f"https://t.me/{BOT_USERNAME}?startgroup=true"
             ),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
@@ -24,7 +25,7 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
